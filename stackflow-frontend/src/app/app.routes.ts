69f558d976dd69c8e1edcc91fd9c1a1a7ad3e971
Kushal Sharma <kushalsharma7092@gmail.com>
2026-09-16 14:57:02 +0530

    feat: configure Angular routes and pages

--- stackflow-frontend/src/app/app.routes.ts
+++ stackflow-frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { EmployeeDetails } from './components/employee-details/employee-details';
+import { EmployeeForm } from './components/employee-form/employee-form';
+import { EmployeeList } from './components/employee-list/employee-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'employees', component: EmployeeList },
+  { path: 'employees/add', component: EmployeeForm },
+  { path: 'employees/:id/edit', component: EmployeeForm },
+  { path: 'employees/:id', component: EmployeeDetails },
+  { path: '**', redirectTo: 'dashboard' },
+];
---
+++ stackflow-frontend/src/app/components/employee-details/employee-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-employee-details',
+  styleUrl: './employee-details.css',
+  templateUrl: './employee-details.html',
+})
+export class EmployeeDetails {}
