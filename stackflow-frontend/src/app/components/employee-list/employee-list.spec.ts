2ecb2bcaf96e54442565780aec341ad7eb3e6d39
Kushal Sharma <kushalsharma7092@gmail.com>
2026-09-16 17:36:59 +0530

    feat: build employee list

--- stackflow-frontend/src/app/components/employee-list/employee-list.spec.ts
+++ stackflow-frontend/src/app/components/employee-list/employee-list.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 import { EmployeeList } from './employee-list';
 
 describe('EmployeeList', () => {
@@ -8,6 +11,7 @@ describe('EmployeeList', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EmployeeList],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmployeeList);
--- stackflow-frontend/src/app/components/employee-list/employee-list.ts
+++ stackflow-frontend/src/app/components/employee-list/employee-list.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { finalize } from 'rxjs';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
-  imports: [],
+  imports: [DecimalPipe, RouterLink],
   selector: 'app-employee-list',
   styleUrl: './employee-list.css',
   templateUrl: './employee-list.html',
 })
-export class EmployeeList {}
+export class EmployeeList implements OnInit {
+  private readonly employeeService = inject(EmployeeService);
+
+  protected readonly employees = signal<Employee[]>([]);
+  protected readonly isLoading = signal(true);
+  protected readonly errorMessage = signal('');
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  protected loadEmployees(): void {
+    this.isLoading.set(true);
+    this.errorMessage.set('');
+
+    this.employeeService
+      .getEmployees()
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (employees) => this.employees.set(employees),
+        error: (error: HttpErrorResponse) => {
+          const message =
+            error.status === 0
+              ? 'The API is unavailable. Confirm that the .NET backend is running.'
+              : 'Employees could not be loaded. Please try again.';
+
+          this.errorMessage.set(message);
+        },
+      });
+  }
+}
